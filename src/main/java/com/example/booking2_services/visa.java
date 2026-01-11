9df9d8986cc63d96dcd9bc90dbdb433da2cdbac7
chaitanyapatil1311-spec <chaitanyapatil1311@gmail.com>
2026-01-11 19:06:53 +0530

    new file added of visa

--- src/main/java/com/example/booking2_services/visa.java
+++ src/main/java/com/example/booking2_services/visa.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class railway {
-    @GetMapping("/railway")
+public class visa {
+    @GetMapping("/visa")
     public String getData() {
-        return "Please book of railway ticket 30% discount" ;
+        return "Please book visa ticket 30% discount" ;
     }
 }
